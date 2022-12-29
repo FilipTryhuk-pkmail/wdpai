6d90ec3df76fa6ef4170f2abe2d367e936487639
Filip <filip.tryhuk@student.pk.edu.pl>
2022-12-29 19:57:13 +0100

    implemented basic PHP routing

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo "Hello world!";
+require 'Routing.php';
+
+$path = parse_url(trim($_SERVER['REQUEST_URI'], '/'), PHP_URL_PATH);
+
+Routing::get('login', 'DefaultController');
+Routing::get('history', 'DefaultController');
+
+Routing::run($path);
+
